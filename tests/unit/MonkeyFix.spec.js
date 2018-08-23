bf2a914c2a73c89c1e54d5fb801132262bd462db
Krzysztof Kowalczyk <kkowalczyk@dan-poltherm.pl>
2018-08-23 14:22:48 +0200

    Added reproduction

---
+++ tests/unit/MonkeyFix.spec.js
@@ -0,0 +1,42 @@
+import { mount } from '@vue/test-utils'
+import App from '@/App.vue'
+import flushPromises from 'flush-promises'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import VeeValidate from 'vee-validate'
+
+Vue.use(Vuetify)
+Vue.use(VeeValidate)
+
+const { getComputedStyle } = window
+window.getComputedStyle = function getComputedStyleStub(el) {
+	return {
+		...getComputedStyle(el),
+		transitionDelay: '',
+		transitionDuration: '',
+		animationDelay: '',
+		animationDuration: ''
+	}
+}
+
+console.log('Vuetify ver.:' + Vuetify.version)
+
+describe('App.vue', () => {
+	it('Adds error on empty field', async () => {
+		const wrapper = mount(App, { sync: false })
+		expect(wrapper.vm.errors.collect('text').length).toBe(0)
+		wrapper.vm.validate()
+		await flushPromises()
+		expect(wrapper.vm.errors.collect('text').length).toBeGreaterThan(0)
+	})
+
+	it('No error on filled field', async () => {
+		const wrapper = mount(App, { sync: false })
+		expect(wrapper.vm.errors.collect('text').length).toBe(0)
+		wrapper.setData({ text: 'abc' })
+		await flushPromises()
+		wrapper.vm.validate()
+		await flushPromises()
+		expect(wrapper.vm.errors.collect('text').length).toBe(0)
+	})
+})
